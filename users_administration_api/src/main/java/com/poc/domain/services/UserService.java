c517200c6fc441ebccf40a6903f66b2498544d19
muhamed-hassan <mohamed.hassan.work@gmail.com>
2022-12-03 13:33:08 +0200

    add business logic

---
+++ users_administration_api/src/main/java/com/poc/domain/services/UserService.java
@@ -0,0 +1,43 @@
+package com.poc.domain.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.poc.persistence.entities.User;
+import com.poc.persistence.repositories.SystemActorRepository;
+import com.poc.persistence.repositories.UserRepository;
+import com.poc.interfaces.rest.models.NewUser;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private SystemActorRepository systemActorRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public void register(NewUser newUser) {
+		var systemActor = systemActorRepository.findById(newUser.getSystemActorId())
+												.orElseThrow(() -> new RuntimeException("Invalid systemActorId"));
+		
+		var user = new User();
+		user.setUsername(newUser.getUsername());
+		user.setPassword(passwordEncoder.encode(newUser.getPassword()));
+		user.setName(newUser.getName());
+		user.setEmail(newUser.getEmail());
+		user.setMobile(newUser.getMobile());
+		user.setSystemActor(systemActor);
+		user.setAccountNonExpired(true);
+		user.setAccountNonLocked(true);
+		user.setCredentialsNonExpired(true);
+		user.setEnabled(true);
+		
+		userRepository.save(user);
+	}
+	
+}
---
+++ users_administration_api/src/main/java/com/poc/interfaces/rest/models/NewUser.java
@@ -0,0 +1,65 @@
+package com.poc.interfaces.rest.models;
+
+public class NewUser {
+	
+	private String username;
+	
+	private String password;
+	
+	private String name;
+	
+	private String mobile;
+	
+	private String email;
+    
+    private int systemActorId;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getSystemActorId() {
+		return systemActorId;
+	}
+
+	public void setSystemActorId(int systemActorId) {
+		this.systemActorId = systemActorId;
+	}
+
+}
