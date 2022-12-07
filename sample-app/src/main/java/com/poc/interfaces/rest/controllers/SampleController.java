82bcb77aaea3a9102c863ffb24ea8842b5ef276e
muhamed-hassan <mohamed.hassan.work@gmail.com>
2022-12-07 10:58:20 +0200

    add more endpoints

--- sample-app/src/main/java/com/poc/interfaces/rest/controllers/SampleController.java
+++ sample-app/src/main/java/com/poc/interfaces/rest/controllers/SampleController.java
@@ -3,6 +3,10 @@ package com.poc.interfaces.rest.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,26 @@ public class SampleController {
 	@PostMapping
 	public ResponseEntity<Void> post(@RequestBody Message message) {
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@PreAuthorize("hasRole('user_a')")
+	@GetMapping
+	public ResponseEntity<Message> get() {
+		var message = new Message();
+		message.setBody(SecurityContextHolder.getContext().getAuthentication().getName());		
+		return ResponseEntity.status(HttpStatus.OK).body(message);
+	}
+	
+	@PreAuthorize("hasRole('user_b')")
+	@DeleteMapping
+	public ResponseEntity<Void> delete() {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	@PreAuthorize("hasRole('user_b')")
+	@PatchMapping
+	public ResponseEntity<Void> patch(@RequestBody Message message) {	
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 	
 }
