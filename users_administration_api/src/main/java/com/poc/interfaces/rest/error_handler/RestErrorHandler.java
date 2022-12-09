a42baaf87d6b5dc9c41ce61309e27d359ac4eaee
muhamed-hassan <mohamed.hassan.work@gmail.com>
2022-12-09 12:40:17 +0200

    code refactoring

--- users_administration_api/src/main/java/com/poc/interfaces/rest/error_handler/RestErrorHandler.java
+++ users_administration_api/src/main/java/com/poc/interfaces/rest/error_handler/RestErrorHandler.java
@@ -2,9 +2,12 @@ package com.poc.interfaces.rest.error_handler;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,7 +17,18 @@ public class RestErrorHandler {
 	private static final String ERROR = "error";
 	
 	@ExceptionHandler
-	public ResponseEntity<Map<String, String>> handleExceptions(Exception exception) {
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		var message = exception.getBindingResult()
+				                .getAllErrors()
+				                .stream()
+				                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+				                .collect(Collectors.joining(", "));
+        var error = Collections.singletonMap(ERROR, message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+    
+	@ExceptionHandler
+	public ResponseEntity<Map<String, String>> handleGeneralExceptions(Exception exception) {
 		var message = exception.getMessage() == null ? 
 				"Unable to process this request." : exception.getMessage();
 		var error = Collections.singletonMap(ERROR, message);
